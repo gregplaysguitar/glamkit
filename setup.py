33025cd7cd09bd798f30c4108f2aea63eca81df8
Thomas Ashelford <thomas@ether.com.au>
2010-04-15 00:09:52 -0600

    setup.py tweaked for pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='glamkit',
     version='0.5',
+    author='Thomas Ashelford',
+    author_email='thomas@interaction.net.au',
     description='A Django toolkit for building websites for the Galleries, Libraries, Archives and Museums sector.',
     long_description=open('README.rst').read(),
     url='http://github.com/glamkit/glamkit',
     packages=find_packages(exclude=[]),
+    package_data={'glamkit': ['docs/*']},
     zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
